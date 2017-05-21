1dbf914db6e49afb777a5e79278612dcb18c7ccb
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2017-05-20 21:05:55 -0500

    que url sigue de crear una solicitud?

---
+++ posgradmin/posgradmin/workflows.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+
+solicitud = {
+        "registrar_curso": "",
+        "solicitar_apoyo_económico": "",
+        "solicitar_baja_tutor": "",
+        "avisar_ausencia": "",
+
+        "seleccionar_jurado": "",
+        "registrar_actividad_complementaria": "",
+        "solicitar_candidatura": "",
+        "cambiar_comite_tutoral": "",
+        "cambiar_titulo_proyecto": "",
+        "cambiar_campo_conocimiento": "",
+        "reportar_suspension": "",
+}
