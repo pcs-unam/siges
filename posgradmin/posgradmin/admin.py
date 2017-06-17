ab164014a8b3a6ed70dafe55129422ebaa994d37
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2017-06-17 14:17:30 -0500

    registra admins para mas clases

--- posgradmin/posgradmin/admin.py
+++ posgradmin/posgradmin/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import Perfil, Academico, Estudiante, Adscripcion,\
     GradoAcademico, Institucion, Entidad, CampoConocimiento, \
-    Beca, Solicitud, Comentario, Anexo
+    Beca, Solicitud, Comentario, Anexo, Proyecto, Dictamen, \
+    Comite
 
 admin.site.register(Perfil)
 admin.site.register(Academico)
@@ -15,3 +16,6 @@ admin.site.register(Beca)
 admin.site.register(Solicitud)
 admin.site.register(Comentario)
 admin.site.register(Anexo)
+admin.site.register(Proyecto)
+admin.site.register(Dictamen)
+admin.site.register(Comite)
