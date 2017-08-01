b40e91d910d044f45c327b2f4a079f64ece7276b
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2017-08-01 12:28:37 -0500

    mejor inicio si se es asistente de proceso

--- posgradmin/posgradmin/admin.py
+++ posgradmin/posgradmin/admin.py
@@ -1,8 +1,14 @@
+# coding: utf-8
 from django.contrib import admin
 from .models import Perfil, Academico, Estudiante, Adscripcion,\
     GradoAcademico, Institucion, Entidad, CampoConocimiento, \
     Beca, Solicitud, Comentario, Anexo, Proyecto, Dictamen, \
     Comite, Asistente, Curso
+
+admin.site.site_header = "Administración de Posgrado en Ciencias de la Sostenibilidad"
+admin.site.site_title = "Posgrado en Ciencias de la Sostenibilidad"
+admin.site.site_url = "/"
+
 
 admin.site.register(Perfil)
 admin.site.register(Academico)
@@ -12,7 +18,7 @@ admin.site.register(GradoAcademico)
 admin.site.register(Institucion)
 admin.site.register(Entidad)
 admin.site.register(CampoConocimiento)
-admin.site.register(Beca)
+# admin.site.register(Beca)
 admin.site.register(Solicitud)
 admin.site.register(Comentario)
 admin.site.register(Anexo)
