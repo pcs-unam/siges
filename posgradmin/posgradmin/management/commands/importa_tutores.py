ec8742dc4a649a9f29d27c07078e14e037cd2e64
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2017-12-02 18:17:15 -0600

    aveces confunde email con lineas

---
+++ posgradmin/posgradmin/management/commands/importa_tutores.py
@@ -0,0 +1,121 @@
+# coding: utf-8
+from django.core.management.base import BaseCommand
+import argparse
+from posgradmin.models import Academico, Institucion, Perfil, Adscripcion, \
+    Solicitud
+from django.contrib.auth.models import User
+import slugify
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+
+import csv
+
+
+class Command(BaseCommand):
+    help = 'Importa académicos desde un archivo CSV'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--csv', type=argparse.FileType('r'),
+                            required=True,
+                            help='archivo CSV con cursos')
+        parser.add_argument('--admin',
+                            required=True,
+                            help='username de admin cargando datos')
+
+    def handle(self, *args, **options):
+        notas = load(options['csv'], options['admin'])
+
+        if notas:
+            orden = ['Grado', 'Apellido Paterno', 'Apellido Materno',
+                     'Nombres', 'Suborganizacion', 'Institucion',
+                     'Líneas de investigación', 'correo electrónico',
+                     'RFC', 'CURP', 'SNI', 'PRIDE', 'CVU',
+                     'NOMBRAMIENTO', 'Tipo']
+
+            print ",".join(orden)
+            for n in notas:
+                print ",".join([n[k] for k in orden]), n['notas']
+
+
+def load(f, admin):
+    admin = User.objects.get(username=admin)
+
+    reader = csv.DictReader(f, delimiter="|", quotechar='"')
+    notas = []
+    for row in reader:
+        # cargar o leer la Institución
+        if "Universidad Nacional Autónoma de México" in row['Institucion'] \
+           or "UNAM" in row['Institucion']:
+            dependencia_unam = True
+        else:
+            dependencia_unam = False
+
+        institucion, creada = Institucion.objects.get_or_create(
+            nombre=row["Institucion"],
+            suborganizacion=row["Suborganizacion"],
+            dependencia_unam=dependencia_unam)
+        if creada:
+            row['notas'] = ["Institución nueva, cargada", ]
+
+        try:
+            validate_email(row['correo electrónico'].decode('utf8'))
+            cuenta = slugify.slugify(
+                row['correo electrónico'].split('@')[0].decode('utf8'))
+        except ValidationError:
+            cuenta = slugify.slugify(u"%s %s %s" % (
+                row['Nombres'].decode('utf8'),
+                row['Apellido Paterno'].decode('utf8'),
+                row['Apellido Materno'].decode('utf8')))
+
+#        try:
+        u = User(username=cuenta,
+                 email=row['correo electrónico'].decode('utf8'))
+        u.first_name = row['Nombres'].decode('utf8')
+        u.last_name = u"%s %s" % (row['Apellido Paterno'].decode('utf8'),
+                                  row['Apellido Materno'].decode('utf8'))
+        u.password = u'pbkdf2_sha256$36000$wAcW7cBkfTcw$AmKBje123fdSHcvz/3PpchHJ+BEcOBe9km1exOvL+123'
+        u.save()
+        # except:
+
+        #     if 'notas' in row:
+        #         row['notas'].append('Imposible crear usuario %s' % cuenta)
+        #     else:
+        #         row['notas'] = ['Imposible crear usuario %s' % cuenta, ]
+        #     notas.append(row)
+        #     continue
+
+        p = Perfil(
+            user=u,
+            curp=row['CURP'],
+            rfc=row['RFC'])
+        p.save()
+
+        s = Solicitud(
+            resumen=u"importar académico %s" % u,
+            tipo="otro",
+            solicitante=admin,
+            descripcion="Solicitud creada automáticamente"
+            + "para la carga de datos",
+            estado="atendida")
+        s.save()
+
+        a = Academico(
+            user=u,
+            titulo=row['Grado'],
+            nivel_pride=row['PRIDE'],
+            nivel_SNI=row['SNI'],
+            CVU=row['CVU'],
+            tutor=True,
+            comite_academico=False,
+            acreditacion=row['Tipo'],
+            lineas=row['Líneas de investigación'],
+            solicitud=s)
+        a.save()
+
+        adscripcion = Adscripcion(
+            academico=a,
+            institucion=institucion,
+            nombramiento=row['NOMBRAMIENTO'])
+        adscripcion.save()
+
+    return notas
