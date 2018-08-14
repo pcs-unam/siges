701b2d7340b6d28de2ef995bf62c7f69e043c679
Rodrigo Garcia <rgarcia@awelo.lancis.ecologia.unam.mx>
2018-08-13 20:32:09 -0500

    orden para cargar fixtures

---
+++ posgradmin/cargar.sh
@@ -0,0 +1,5 @@
+./manage.py carga_academicos --csv /home/rgarcia/golden/academicos.csv
+./manage.py carga_usuarios --csv /home/rgarcia/golden/usuarios_alumnos.csv
+./manage.py carga_usuarios_estudiantes --csv /home/rgarcia/golden/usuario_estudiante.csv
+./manage.py carga_proyectos --csv /home/rgarcia/golden/alumnos_proyectos.csv
+./manage.py carga_comites_tutorales --csv /home/rgarcia/golden/comites_tutorales.csv
