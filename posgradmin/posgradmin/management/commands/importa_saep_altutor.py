1595aa3dd39b3973364f51f64c89ff3dc23e78c2
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2021-08-08 14:02:56 -0500

    busca tutores por curp y por email si rfc falla

--- posgradmin/posgradmin/management/commands/importa_saep_altutor.py
+++ posgradmin/posgradmin/management/commands/importa_saep_altutor.py
@@ -27,26 +27,58 @@ idx = {
     'tipo': 4,
 }
 
+tutor_curp = {}
+tutor_email = {}
 
+
+def busca_tutor(rfc):
+
+    if models.Perfil.objects.filter(
+            rfc__icontains=rfc
+    ).count() == 1:
+        p = models.Perfil.objects.filter(rfc__icontains=rfc)[0]
+    elif models.Perfil.objects.filter(
+            curp__icontains=tutor_curp.get(rfc, 'aguas')
+    ).count() == 1:
+        p = models.Perfil.objects.filter(curp__icontains=tutor_curp[rfc])[0]
+    elif models.User.objects.filter(
+            email__icontains=tutor_email.get(rfc, 'aguas@example.com')
+    ).count() == 1:
+        u = models.User.objects.filter(email__icontains=tutor_email[rfc])[0]
+        if hasattr(u, 'perfil'):
+            p = u.perfil
+        else:
+            return None
+    else:
+        print('nomas no encontre', rfc)
+        return None
+    return p.user.academico
+
+    
+    
 def importa(db_file):
     data = get_data(db_file.name)
 
+    # crea diccionario: llaves son rfc, valores curps y correos
+    for i in range(0, len(data['tutor']) - 1):
+        m = data['tutor'][i]
+        if len(m) == 0:
+            continue
+        elif len(m) > 9:
+            tutor_email[m[0]] = m[9]            
+        tutor_curp[m[0]] = m[8]
+
+    # busca tutores por rfc
     for i in range(0, len(data['altutor']) - 1):
         m = data['altutor'][i]
         if len(m) == 0:
             continue
 
-        rfc_hits = models.Perfil.objects.filter(
-            rfc__icontains=m[
-                idx['rfc']
-            ]
-        ).count()
+        print('busque', m[idx['rfc']], 'hallazgo', busca_tutor(m[idx['rfc']]))
 
-        if rfc_hits == 1:
-
-            p = models.Perfil.objects.filter(rfc__icontains=m[idx['rfc']])[0]
-
-            a = p.user.academico
+        a = busca_tutor(m[idx['rfc']])
+        
+        if a is not None:
 
             if models.Estudiante.objects.filter(cuenta=m[idx['cuenta']]).count() == 1:
                 e = models.Estudiante.objects.get(cuenta=m[idx['cuenta']])
@@ -66,8 +98,4 @@ def importa(db_file):
             elif models.Estudiante.objects.filter(cuenta=m[idx['cuenta']]).count() > 1:
                 print('Estudiante DUPLICADO', m[idx['cuenta']])
 
-        elif rfc_hits == 0:
-            print('perfil no encontrado', m[idx['rfc']])
 
-        elif rfc_hits > 1:
-            print('perfil con rfc duplicado', m[idx['rfc']])
