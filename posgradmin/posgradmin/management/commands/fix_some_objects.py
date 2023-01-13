bd4820a67f62d29bf4adf1c2db378203d6cb2812
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2023-01-12 20:56:25 -0600

    uso pickles para quedarme el username como llave

--- posgradmin/posgradmin/management/commands/fix_some_objects.py
+++ posgradmin/posgradmin/management/commands/fix_some_objects.py
@@ -3,7 +3,7 @@ from django.core.management.base import BaseCommand
 from posgradmin import models
 from os import path
 from django.core import serializers
-
+import pickle
 
 
 class Command(BaseCommand):
@@ -17,18 +17,75 @@ class Command(BaseCommand):
 def fix_some():
 
     # fijar usuarios con estudiante
-    data = [u for u in models.User.objects.all()
+    users = [u for u in models.User.objects.all()
             if hasattr( u, 'estudiante')]
 
+    estudiantes = {e.user.username:e for e in models.Estudiante.objects.all()}
+    academicos = {}
+    acreditaciones = {}
+    
     # que no sean estudiantes ni académicos
-    data += [u for u in models.User.objects.all()
+    users += [u for u in models.User.objects.all()
              if not hasattr( u, 'academico')
              and not hasattr(u, 'estudiante')]
-    
-    with open("../import_2023/undelete_me.json", "w") as out:
-        serializers.serialize("json", data, stream=out)
-    
+
+    for uid in [390,
+                476,
+                719,
+                720,
+                721,
+                723,
+                840,]:
+        u = models.User.objects.get(pk=uid)
+        users.append(u)
+        if hasattr(u, 'estudiante'):
+            estudiantes[u.username] = u.estudiante
+
+        if hasattr(u, 'academico'):
+            academicos[u.username] = u.academico
+            acreditaciones[u.username] = []
+            for ac in u.academico.acreditaciones.all():
+                acreditaciones[u.username].append(ac)
+
+    grados = {}
+    for u in users:
+        for a in u.gradoacademico_set.all():
+            if u.username in grados:
+                grados[u.username].append(a)
+            else:
+                grados[u.username] = [a, ]
+
+    perfiles = {u.username: u.perfil for u in users
+                if hasattr(u, 'perfil')}
+
+    adscripciones = {}
+    for username in perfiles:
+        p = perfiles[username]
+        for a in p.adscripcion_set.all():
+            if username in adscripciones:
+                adscripciones[username].append(a)
+            else:
+                adscripciones[username] = [a, ]
 
 
 
+    with open("../import_2023/undelete_users.json", "w") as out:
+        serializers.serialize("json", users, stream=out)
 
+    with open("../import_2023/undelete_estudiantes.pickle", "wb") as out:
+        pickle.dump(estudiantes, out)
+
+    with open("../import_2023/undelete_academicos.pickle", "wb") as out:
+        pickle.dump(academicos, out)
+
+    with open("../import_2023/undelete_acreditaciones.pickle", "wb") as out:
+        pickle.dump(acreditaciones, out)
+        
+    with open("../import_2023/undelete_perfiles.pickle", "wb") as out:
+        pickle.dump(perfiles, out)
+
+    with open("../import_2023/undelete_grados.pickle", "wb") as out:
+        pickle.dump(grados, out)
+
+    with open("../import_2023/undelete_adscripciones.pickle", "wb") as out:
+        pickle.dump(adscripciones, out)
--- posgradmin/posgradmin/management/commands/undelete_some_objects.py
+++ posgradmin/posgradmin/management/commands/undelete_some_objects.py
@@ -3,7 +3,8 @@ from django.core.management.base import BaseCommand
 from posgradmin import models
 from os import path
 from django.core import serializers
-
+import jellyfish
+import pickle
 
 class Command(BaseCommand):
     help = u'Carga algunos objetos en archivos json a la base de datos. Ver fix_some_objects.'
@@ -12,14 +13,93 @@ class Command(BaseCommand):
         undelete_some()
 
 
+def search_similar_names(u):
+    u_name = u.get_full_name()
+    if not u_name:
+        return
+
+    similar = []
+    for user in models.User.objects.all():
+        name = user.get_full_name()
+
+        d = jellyfish.levenshtein_distance(u_name, name)
+        if d <= 2 and u.id != user.id:
+            similar.append(user)
+
+    return similar
+
+
+        
+def undelete_users():
+    for f in serializers.deserialize('json',
+                                     open(f"../import_2023/undelete_users.json").read()):
+        u = f.object
+            
+        if models.User.objects.filter(username=u.username).count() == 0:
+            u.save()
+            print(f'undeleted user {u}', u.id)
+            dupes = search_similar_names(u)
+            if dupes:
+                print(u, dupes)
+            print('\n')
+    
+
+def undelete_grados():
+    with open('../import_2023/undelete_grados.pickle', 'rb') as f:
+        grados = pickle.load(f)
+        for username in grados:
+            for g in grados[username]:
+            
+                if models.User.objects.filter(username=username).count() == 1:
+                    u_db = models.User.objects.get(username=username)
+                    g.user = u_db
+                    g.save()
+                    print(f'undeleted grado {u_db} {g}')
+    
+
+def undelete_academicos():
+    with open('../import_2023/undelete_academicos.pickle', 'rb') as f:
+        academicos = pickle.load(f)
+    for username in academicos:
+        a = academicos[username]
+            
+        if models.User.objects.filter(username=username).count() == 1:
+            u_db = models.User.objects.get(username=username)
+            a.user = u_db
+            a.save()
+            print(f'undeleted academico {u_db} {a}')
+
+            
+def undelete_acreditaciones():
+    with open('../import_2023/undelete_acreditaciones.pickle', 'rb') as f:
+        acreditaciones = pickle.load(f)
+    for username in acreditaciones:
+        if models.User.objects.filter(username=username).count() == 1:
+            u_db = models.User.objects.get(username=username)
+            academico = u_db.academico
+        
+            for a in acreditaciones[username]:
+                a.academico = academico
+                a.save()
+                print(f'undeleted acreditacion {academico} {a}')
+
+
+def undelete_adscripciones():
+    with open('../import_2023/undelete_adscripciones.pickle', 'rb') as f:
+        ads = pickle.load(f)
+    for username in ads:
+        if models.User.objects.filter(username=username).count() == 1:
+            u_db = models.User.objects.get(username=username)
+            for a in ads[username]:
+                a.user = u_db
+                a.save()
+                print(f'undeleted adscripcion {u_db} {a}')
+
 
 def undelete_some():
 
-    for u in serializers.deserialize('json',
-                                     open("../import_2023/undelete_me.json").read()):
-            
-        if models.User.objects.filter(username=u.object.username).count() == 0:
-            print('undeleting', u.object.username)
-            u.save()
-
-    
+    undelete_users()
+    undelete_grados()
+    undelete_academicos()
+    undelete_acreditaciones()
+#    undelete_adscripciones()
