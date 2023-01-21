235bbd6d97718459462a8453f1ee3008cedfacaf
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2023-01-20 21:27:37 -0600

    para actualizar membresias con archivo saep v C

--- posgradmin/posgradmin/management/commands/importa_saep_altutor.py
+++ posgradmin/posgradmin/management/commands/importa_saep_altutor.py
@@ -33,74 +33,50 @@ tutor_email = {}
 
 def busca_tutor(rfc):
 
-    if models.Perfil.objects.filter(
-            rfc__icontains=rfc
-    ).count() == 1:
-        p = models.Perfil.objects.filter(rfc__icontains=rfc)[0]
-    elif models.Perfil.objects.filter(
-            curp__icontains=tutor_curp.get(rfc, 'aguas')
-    ).count() == 1:
-        p = models.Perfil.objects.filter(curp__icontains=tutor_curp[rfc])[0]
-    elif models.User.objects.filter(
-            email__icontains=tutor_email.get(rfc, 'aguas@example.com')
-    ).count() == 1:
-        u = models.User.objects.filter(email__icontains=tutor_email[rfc])[0]
-        if hasattr(u, 'perfil'):
-            p = u.perfil
-        else:
-            return None
-    else:
-        print('nomas no encontre', rfc)
-        return None
-    return p.user.academico
+    p = models.Perfil.objects.filter(rfc__icontains=rfc).first()
 
+    if p:
+        if  hasattr(p.user, 'academico'):
+            return p.user.academico
+
+    return None
+
+
+def busca_estudiante(cuenta):
+    return models.Estudiante.objects.filter(cuenta=cuenta).first()
 
 
 def importa(db_file):
     data = get_data(db_file.name)
 
-    models.MembresiaComite.objects.all().delete()
 
-    # crea diccionario: llaves son rfc, valores curps y correos
-    for i in range(0, len(data['tutor']) - 1):
-        m = data['tutor'][i]
-        if len(m) == 0:
+    for i in range(0, len(data['altutor'])):
+        row = data['altutor'][i]
+
+        # generar indice de columnas usando el encabezado
+        if i == 0:
+            idx = {f: row.index(f)
+                   for f in row}
             continue
-        elif len(m) > 9:
-            tutor_email[m[0]] = m[9]
-        tutor_curp[m[0]] = m[8]
-
-    # busca tutores por rfc
-    for i in range(0, len(data['altutor']) - 1):
-        m = data['altutor'][i]
-        if len(m) == 0:
+        elif len(row) == 0:
+            # descartar lineas vacias
             continue
 
-        print('busque', m[idx['rfc']], 'hallazgo', busca_tutor(m[idx['rfc']]))
 
-        a = busca_tutor(m[idx['rfc']])
+        # anio	semestre	cuenta	rfc	tipo
+        tutor = busca_tutor(row[idx['rfc']])
+        estudiante = busca_estudiante(row[idx['cuenta']])
 
-        if a is not None:
+        if tutor and estudiante:
+            m, created = models.MembresiaComite.objects.get_or_create(
+                estudiante=estudiante,
+                tutor=tutor,
+                year=row[idx['anio']],
+                semestre=row[idx['semestre']],
+                tipo=row[idx['tipo']])
 
-            if models.Estudiante.objects.filter(cuenta=m[idx['cuenta']]).count() == 1:
-                e = models.Estudiante.objects.get(cuenta=m[idx['cuenta']])
+            if created:
+                print('membresia creada', m)
+            else:
+                print('membresia ya existente', m)
 
-                if models.MembresiaComite.objects.filter(
-                        estudiante=e,
-                        tutor=a,
-                        year=m[idx['anio']],
-                        semestre=m[idx['semestre']],
-                        tipo=m[idx['tipo']]).count() == 0:
-                    membresia = models.MembresiaComite(
-                        estudiante=e,
-                        tutor=a,
-                        year=m[idx['anio']],
-                        semestre=m[idx['semestre']],
-                        tipo=m[idx['tipo']])
-                    membresia.save()
-                    print('membresia creada', membresia)
-                else:
-                    print('membresia ya existente', e, a)
-
-            elif models.Estudiante.objects.filter(cuenta=m[idx['cuenta']]).count() > 1:
-                print('Estudiante DUPLICADO', m[idx['cuenta']])
